07d9b8d0940b1462467999e657fa2fef1e6c8995
SeitaKND <udonkoseita0204@gmail.com>
2026-01-10 19:12:21 +0900

    first commit

---
+++ basic/01_function.js
@@ -0,0 +1,10 @@
+function greet(name){
+    return `hello,${name}!`
+}
+
+function add(x,y){
+    return x+y
+}
+
+console.log(greet("kondo"))
+console.log(add(5,20))
---
+++ basic/02_forArray.js
@@ -0,0 +1,9 @@
+        const scores=[36,40,53,75,92]
+        let num=0
+
+        for (let i=0;i<scores.length;i++){
+            num=num+scores[i]
+            console.log(num)
+        }
+
+        console.log(num)
