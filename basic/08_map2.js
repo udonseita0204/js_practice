e47a51c96421f5b960d7a6183c737ecc792d29b5
SeitaKND <udonkoseita0204@gmail.com>
2026-01-14 00:04:54 +0900

    配列の生成と操作を修正し、要素を倍にし、偶数を100倍する機能を追加

--- basic/08_map2.js
+++ basic/08_map2.js
@@ -2,12 +2,31 @@
 const getRandomInt=()=>Math.floor(Math.random()*100)
 
 //loop回数の指定
-const loop=10;
-let ArrayInt=[];
+const loop=5;
+let ArraysInt=[];
 
 //loop回数分だけ配列を生成
 for(i=0;i<loop;i++){
-    ArrayInt[i]=getRandomInt();
+    ArraysInt[i]=getRandomInt();
 }
 
-console.log(ArrayInt)
+console.log(ArraysInt)
+
+//配列の各要素を倍にする変数を定義
+const doubled=ArraysInt.map(array=>array*2);
+
+console.log(doubled)
+
+
+//２の倍数のみ100倍する
+const Evenfil=ArraysInt.map(
+    function (array){
+        if(array%2===0){
+            return array*100;
+        }
+        else{
+            return array;
+        }
+    }
+)
+console.log(Evenfil)
